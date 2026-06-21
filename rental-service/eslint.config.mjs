abedbd1dfd982c323075a6bfb435f017aeca163b
Val <valentinmenon5@gmail.com>
2026-06-21 21:45:26 +0200

    Refactor ESLint configuration for goat-service and rental-service; update rapport files and images
    - Improved formatting and structure of ESLint configuration files for better readability.
    - Updated references and labels in rapport.aux, rapport.fdb_latexmk, rapport.fls, rapport.log, rapport.pdf, rapport.tex, rapport.toc, and rapport.synctex.gz to reflect changes in content and structure.
    - Adjusted image references and sizes in rapport.pdf to ensure correct display.
    - Enhanced clarity in the rapport.tex document by refining text descriptions and removing unnecessary placeholders.

--- rental-service/eslint.config.mjs
+++ rental-service/eslint.config.mjs
@@ -4,7 +4,25 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" }, globals: { ...globals.jest } },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
 ]);
